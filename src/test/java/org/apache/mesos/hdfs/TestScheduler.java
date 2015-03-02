e9c6a396ba8b00bfb967173730864b84b628249c
Abhay Agarwal <abhay.agarwal@berkeley.edu>
2015-03-01 18:03:34 -0800

    updated task to pass

--- src/test/java/org/apache/mesos/hdfs/TestScheduler.java
+++ src/test/java/org/apache/mesos/hdfs/TestScheduler.java
@@ -99,8 +99,8 @@ public class TestScheduler {
             )
         );
 
-    verify(liveState, times(1)).addTask(any(Protos.TaskID.class),
-        eq(createTestOffer(0).getHostname()), eq(createTestOffer(0).getSlaveId().getValue()));
+    verify(liveState, times(7)).addTask(any(Protos.TaskID.class),
+        anyString(), anyString());
   }
 
   private Protos.TaskID createTaskId(String id) {
