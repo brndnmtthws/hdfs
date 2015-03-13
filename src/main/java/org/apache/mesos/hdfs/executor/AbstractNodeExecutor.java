20019f51bf39632da7f58aa5f2b68c6f10221f44
elingg <elingg@cs.stanford.edu>
2015-03-13 16:11:35 -0700

    Fix debug statement

--- src/main/java/org/apache/mesos/hdfs/executor/AbstractNodeExecutor.java
+++ src/main/java/org/apache/mesos/hdfs/executor/AbstractNodeExecutor.java
@@ -118,7 +118,7 @@ public abstract class AbstractNodeExecutor implements Executor {
           log.info("Unable to unlink old sym link. Link may not exist. Exit code: " + exitCode);
         }
       } catch (IOException e) {
-        log.fatal("Could not unlink" + hdfsBinary + ": " + e);
+        log.fatal("Could not unlink" + hdfsBinaryPath + ": " + e);
         System.exit(1);
       }
 
